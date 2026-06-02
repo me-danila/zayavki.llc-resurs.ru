68214738c4b5e3b39ad9135f71dca890a72c66bf
admin <daniil.cech@gmail.com>
2026-06-02 16:11:08 +0600

    placeholder update

--- src/App.tsx
+++ src/App.tsx
@@ -116,7 +116,7 @@ const App: React.FC = () => {
               {/* Номер заявки */}
               <div>
                 <label className="text-[11px] uppercase font-bold text-gray-500 mb-1.5 block">Номер заявки</label>
-                <input type="text" {...register('requestNo')} className="resource-input text-sm" placeholder="Например, Б001" />
+                <input type="text" {...register('requestNo')} className="resource-input text-sm" placeholder="Например, Б125" />
                 {errors.requestNo && <p className="mt-1 text-xs text-red-500">{errors.requestNo.message}</p>}
               </div>
 
